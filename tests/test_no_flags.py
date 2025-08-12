0eae22216c914ab6d5864fbbeb6503c8af50403c
Shishir Sudeesh Nambiar <shishirnam11@gmail.com>
2025-08-12 13:26:03 +0530

    Remove obsolete tests for currency description without flags

--- tests/test_no_flags.py
+++
@@ -1,9 +0,0 @@
-from currency_converter.currencies import describe, get_flag
-
-print("Testing currency descriptions without flags:")
-print("USD:", describe('USD'))
-print("EUR:", describe('EUR'))
-print("GBP:", describe('GBP'))
-
-print("\nFlag function still works (but not used in describe):")
-print("USD flag:", get_flag('USD'))
